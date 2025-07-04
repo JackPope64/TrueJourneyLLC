7e865cb492d8317cd6886f702e09222fb1392fc6
ByramDigitalMedia <contact@byramdigitalmedia.com>
2025-07-04 10:51:09 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/* =======================================================
+  * Template Name: BDM date.js
+  * Template URL: https://byramdigitalmedia.com
+  * Updated: Jun 19 2025 with Bootstrap v5.3.3
+  * Author: BDM
+  * License: MIT
+  ======================================================== */
+
+let date = new Date().getFullYear()
+document.getElementById("year").innerHTML = date
